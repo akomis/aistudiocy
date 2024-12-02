71d4c8a076b8335f21d43479e8f29e9252ff990c
akomis <akomis@protonmail.com>
2024-12-02 15:16:41 +0200

    add about and socials

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import useStrapi from "@/hooks/useStrapi";
 import Screen from "../components/Screen";
 import Section from "../components/Section";
-import { ApiAboutAbout } from "@/types/generated/contentTypes";
+import { ApiAboutAbout, ApiSocialSocial } from "@/types/generated/contentTypes";
 import { StrapiAttributes } from "@/types/strapi";
 import Block from "@/components/Block";
 
@@ -14,20 +14,41 @@ const About = () => {
 
   const abouts = data.data as StrapiAttributes<ApiAboutAbout>;
 
-  console.log(abouts);
-
   return (
-    <Section className="py-10 border-2 border-red-500">
+    <Section>
       <div className="flex gap-20 items-end">
-        <p className="text-4xl">ABOUT</p>
-        <div>
+        <p className="text-9xl font-bold">ABOUT</p>
+        <div className="flex flex-col gap-10 m-10">
           {abouts.map((about) => (
-            <div key={about.Title}>
-              <p>{about.Title}</p>
-              <Block content={about?.Content} />
+            <div key={about.Title} className="flex flex-col gap-2">
+              <p className="text-4xl font-bold">{about.Title}</p>
+              <Block content={about.Content} />
             </div>
           ))}
         </div>
+      </div>
+    </Section>
+  );
+};
+
+const Footer = () => {
+  const { data } = useStrapi("socials");
+
+  if (!data) return null;
+
+  const socials = data.data as StrapiAttributes<ApiSocialSocial>;
+
+  return (
+    <Section className="h-[200px] flex justify-end items-end">
+      <div className="flex font-bold gap-10 mr-10 mb-10">
+        {socials.map((social) => (
+          <div key={social.Key} className="flex flex-col">
+            <p>{social.Key}</p>
+            <a href={social.URL} target="_blank">
+              {social.Value}
+            </a>
+          </div>
+        ))}
       </div>
     </Section>
   );
@@ -40,9 +61,7 @@ export default function Home() {
         <div>HEADER</div>
       </Section> */}
       <About />
-      {/* <Section>
-        <div>FOOTER</div>
-      </Section> */}
+      <Footer />
     </Screen>
   );
 }

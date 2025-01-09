c319056f3d026899627d9ef09a851bb568286f17
akomis <akomis@protonmail.com>
2025-01-09 21:55:53 +0200

    make region an environment variable

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const REGION_ID = "reg_01JE7JJ691K3XF415A25MVP6ZT";
+export const REGION_ID = process.env.NEXT_PUBLIC_REGION_ID;
 
 export const PHONE_REGEX = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/

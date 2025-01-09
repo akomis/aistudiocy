ce01ba2ba92bd4c367788874989338b2706c7f01
akomis <akomis@protonmail.com>
2025-01-09 22:07:35 +0200

    fixes

--- frontend/src/components/DynamicBackground.tsx
+++ frontend/src/components/DynamicBackground.tsx
@@ -16,7 +16,6 @@ const DynamicBackground = ({ data }: Props) => {
 
   const router = useRouter();
   const categories = data.map((e) => e.category);
-  //const assets = data.map((e) => ({ type: "image", url: e.url }));
 
   useEffect(() => {
     if (isPaused) return;
--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const REGION_ID = process.env.NEXT_PUBLIC_REGION_ID;
+export const REGION_ID = process.env.NEXT_PUBLIC_REGION_ID as string;
 
 export const PHONE_REGEX = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/

19e17ed2c5a8d093030e5d8fb5358d5e5e88aeb0
akomis <akomis@protonmail.com>
2025-12-11 15:53:57 +0200

    fix redirect to ring size page

--- src/components/RingSizeGuide.tsx
+++ src/components/RingSizeGuide.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function RingSizeGuide() {
   return (
     <Link
-      href="/sizing"
+      href="/ring-size"
       target="_blank"
       className="text-2xl md:text-4xl font-bold hover:cursor-pointer hover:opacity-75 transition-all"
     >
       RING SIZE GUIDE
     </Link>
-  )
+  );
 }
